ae2ed2f561e839ca5c6de7a07523d64046abbb3b
lcmpbll <105952041+lcmpbll@users.noreply.github.com>
2022-08-08 14:03:57 -0700

    Initial commit

---
+++ ProjectName/Models/ProjectNameContext.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace ProjectName.Models
+{
+  public class ProjectNameContext : DbContext
+  {
+    public DbSet<CLassOne> ClassOne { get; set; }
+    public DbSet<ClassTwo> ClassTwo { get; set; }
+    public DbSet<ClassOneClassTwo> ClassOneClassTwo { get;  set; }
+
+    public ProjectNameContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseLazyLoadingProxies();
+    }
+  }
+}
