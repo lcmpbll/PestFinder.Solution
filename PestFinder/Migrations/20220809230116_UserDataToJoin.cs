ce29085c199a4a51acebcf55f5a8bc36e8f7e903
Liam Cammpbell <liam22campbell@gmail.com>
2022-08-09 16:08:16 -0700

    Add create user data to pestlocation
    xCo-authored-by: Mo Byrne <mosey.b.byrne@gmail.com>
    Co-authored-by: Anthony DiFalco <aodifalco@gmail.com>

---
+++ PestFinder/Migrations/20220809230116_UserDataToJoin.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PestFinder.Migrations
+{
+    public partial class UserDataToJoin : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "PestLocation",
+                type: "varchar(255) CHARACTER SET utf8mb4",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PestLocation_UserId",
+                table: "PestLocation",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PestLocation_AspNetUsers_UserId",
+                table: "PestLocation",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PestLocation_AspNetUsers_UserId",
+                table: "PestLocation");
+
+            migrationBuilder.DropIndex(
+                name: "IX_PestLocation_UserId",
+                table: "PestLocation");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "PestLocation");
+        }
+    }
+}
